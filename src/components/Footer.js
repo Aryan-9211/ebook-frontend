5c66b034ee7d03d478bb70a688d25f86a9f6ae6e
Aryan-9211 <manglaaryan178@gmail.com>
2023-04-21 15:53:12 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBIcon,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+
+const Footer = () =>{
+  return (
+    <MDBFooter className='bg-light text-center text-white'>
+      <MDBContainer className='p-4 pb-0'>
+        <section className='mb-4'>
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#3b5998' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='facebook-f' />
+          </MDBBtn>
+
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#55acee' }}
+            href='https://www.twitter.com/aryanchy7601'
+            role='button'
+            target='_blank'
+          >
+            <MDBIcon fab icon='twitter' />
+          </MDBBtn>
+
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#dd4b39' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='google' />
+          </MDBBtn>
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#ac2bac' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='instagram' />
+          </MDBBtn>
+
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#0082ca' }}
+            href='https://www.linkedin.com/in/aryan183'
+            role='button'
+            target='_blank'
+          >
+            <MDBIcon fab icon='linkedin-in' />
+          </MDBBtn>
+
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#333333' }}
+            href='https://www.github.com/183aryan'
+            role='button'
+            target='_blank'
+          >
+            <MDBIcon fab icon='github' />
+          </MDBBtn>
+        </section>
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2023 Copyright:
+        <a className='text-white' href='/'>
+          All rights reserved
+        </a>
+        <p>Made with ❤ by <a href='https://www.github.com/183aryan' target='_blank'>ARYAN</a></p>
+      </div>
+    </MDBFooter>
+  );
+}
+
+export default Footer;
